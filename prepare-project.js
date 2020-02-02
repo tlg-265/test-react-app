21bbf986215200b2be42a22392534420801ac295
Angel Hernandez <angel@theleadgroup.com>
2020-02-02 14:58:28 -0600

    using ENV variables instead

--- prepare-project.js
+++ prepare-project.js
@@ -1,16 +1,4 @@
-const yargs = require('yargs');
-
-const argv = yargs
-    .option('project', {
-        alias: 'p',
-        description: 'name of the project',
-        type: 'string',
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
-
-let project = argv.project;
+let project = process.env.P;
 
 console.log(`########################`);
 console.log(`###### The current project is: ${project} ######`);
