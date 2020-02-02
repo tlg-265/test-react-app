5b5e3c4db28dbcadbe10bb73a65babd9a5d8b1fe
Angel Hernandez <angel@theleadgroup.com>
2020-02-02 10:06:07 -0600

    added: prepare-project.js

---
+++ prepare-project.js
@@ -0,0 +1,5 @@
+let project = 'ferrari' // replace this value with the one passed via: $ npm start [what to put here?]
+
+console.log(`########################`);
+console.log(`###### The current project is: ${project} ######`);
+console.log(`########################`);
